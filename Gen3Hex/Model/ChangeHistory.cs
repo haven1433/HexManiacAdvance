44b717ef9a4820d6b55f7354f21c94f0dcd106eb
Benjamin Popp <haven1433@gmail.com>
2018-10-16 22:26:58 -0500

    adding undo/redo manager object, named ChangeHistory. Added tests showing how it should work.

---
+++ Gen3Hex/Model/ChangeHistory.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace HavenSoft.Gen3Hex.Model {
+   /// <summary>
+   /// Represents a history of changes that can undo / redo.
+   /// The change can be reperesented by any class with an empty constructor.
+   /// New change objects will be created automatically as needed.
+   /// The user is responsible for using the change object to revert a change via the constructor delegate call.
+   /// The user is responsible for converting from a backward change object to a forward change (redo) object.
+   /// The user is responsible for assigning boundaries between changes by calling ChangeCompleted.
+   /// </summary>
+   public class ChangeHistory<T> where T : class, new() {
+      private readonly Func<T, T> revert;
+      private readonly StubCommand undo, redo;
+      private readonly Stack<T>
+         undoStack = new Stack<T>(),
+         redoStack = new Stack<T>();
+
+      private T currentChange;
+
+      public ICommand Undo => undo;
+      public ICommand Redo => redo;
+      public T CurrentChange {
+         get {
+            if (redoStack.Count > 0) {
+               redoStack.Clear();
+               redo.CanExecuteChanged.Invoke(redo, EventArgs.Empty);
+            }
+
+            if (currentChange == null) {
+               currentChange = new T();
+               if (undoStack.Count == 0) undo.CanExecuteChanged.Invoke(undo, EventArgs.Empty);
+            }
+            return currentChange;
+         }
+      }
+
+      public ChangeHistory(Func<T,T> revertChange) {
+         revert = revertChange;
+         undo = new StubCommand {
+            Execute = arg => UndoExecuted(),
+            CanExecute = arg => undoStack.Count > 0 || currentChange != null,
+         };
+         redo = new StubCommand {
+            Execute = arg => RedoExecuted(),
+            CanExecute = arg => redoStack.Count > 0,
+         };
+      }
+
+      public void ChangeCompleted() {
+         if (currentChange == null) return;
+         undoStack.Push(currentChange);
+         currentChange = null;
+      }
+
+      private void UndoExecuted() {
+         ChangeCompleted();
+         if (undoStack.Count == 0) return;
+
+         var originalChange = undoStack.Pop();
+         if (undoStack.Count == 0) undo.CanExecuteChanged.Invoke(undoStack, EventArgs.Empty);
+         var reverseChange = revert(originalChange);
+         if (currentChange != null) throw new InvalidOperationException("Cannot create a change during an undo.");
+         redoStack.Push(reverseChange);
+         if (redoStack.Count == 1) redo.CanExecuteChanged.Invoke(redo, EventArgs.Empty);
+      }
+
+      private void RedoExecuted() {
+         if (redoStack.Count == 0) return;
+
+         var reverseChange = redoStack.Pop();
+         if (redoStack.Count == 0) redo.CanExecuteChanged.Invoke(redoStack, EventArgs.Empty);
+         var originalChange = revert(reverseChange);
+         if (currentChange != null) throw new InvalidOperationException("Cannot create a change during an redo.");
+         undoStack.Push(originalChange);
+         if (undoStack.Count == 1) undo.CanExecuteChanged.Invoke(undo, EventArgs.Empty);
+      }
+   }
+}
---
+++ HexTests/ChangeHistoryTests.cs
@@ -0,0 +1,142 @@
+﻿using HavenSoft.Gen3Hex.Model;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace HavenSoft.HexTests {
+   public class ChangeHistoryTests {
+
+      private readonly ChangeHistory<List<int>> history;
+      private int callCount = 0;
+      private List<int> recentChanges;
+
+      public ChangeHistoryTests() {
+         history = new ChangeHistory<List<int>>(changes => {
+            callCount++;
+            recentChanges = changes;
+            return changes.Select(i => -i).ToList();
+         });
+      }
+
+      [Fact]
+      public void CannotUndoByDefault() {
+         Assert.False(history.Undo.CanExecute(null));
+      }
+
+      [Fact]
+      public void CallingUndoWhenStackIsEmptyDoesNothing() {
+         history.Undo.Execute(null);
+         history.Redo.Execute(null);
+         Assert.Equal(0, callCount);
+      }
+
+      [Fact]
+      public void CanUndoAfterCheckingCurrentTransaction() {
+         history.CurrentChange.Count();
+         Assert.True(history.Undo.CanExecute(null));
+         Assert.False(history.Redo.CanExecute(null));
+      }
+
+      [Fact]
+      public void UndoCallsRevertDelegate() {
+         history.CurrentChange.Count();
+         history.Undo.Execute(null);
+         Assert.Equal(1, callCount);
+         Assert.False(history.Undo.CanExecute(null));
+         Assert.True(history.Redo.CanExecute(null));
+      }
+
+      [Fact]
+      public void UndoPassesInRecentChanges() {
+         history.CurrentChange.Add(3);
+         history.Undo.Execute(null);
+
+         Assert.NotNull(recentChanges);
+         Assert.Single(recentChanges);
+         Assert.Equal(3, recentChanges[0]);
+      }
+
+      [Fact]
+      public void RedoPassesInRecentChanges() {
+         history.CurrentChange.Add(3);
+         history.Undo.Execute(null);
+         history.Redo.Execute(null);
+
+         Assert.NotNull(recentChanges);
+         Assert.Single(recentChanges);
+         Assert.Equal(-3, recentChanges[0]);
+      }
+
+      [Fact]
+      public void CannotRedoAfterNewChange() {
+         history.CurrentChange.Add(3);
+         history.Undo.Execute(null);
+         history.CurrentChange.Add(4);
+
+         Assert.True(history.Undo.CanExecute(null));
+         Assert.False(history.Redo.CanExecute(null));
+      }
+
+      [Fact]
+      public void ClosingEmptyChangeDoesNotAddUndoItem() {
+         history.ChangeCompleted();
+
+         Assert.False(history.Undo.CanExecute(null));
+      }
+
+      [Fact]
+      public void ClosingChangesAllowsForMultipleUndos() {
+         history.CurrentChange.Add(3);
+         history.ChangeCompleted();
+         history.CurrentChange.Add(4);
+
+         history.Undo.Execute(null);
+         Assert.Equal(4, recentChanges[0]);
+
+         history.Undo.Execute(null);
+         Assert.Equal(3, recentChanges[0]);
+      }
+
+      [Fact]
+      public void UndoCanExecuteChangeFiresCorrectly() {
+         var canExecuteChangeCalled = 0;
+         history.Undo.CanExecuteChanged += (sender, e) => canExecuteChangeCalled++;
+
+         history.CurrentChange.Add(3);
+         Assert.Equal(1, canExecuteChangeCalled);
+
+         history.Undo.Execute(null);
+         Assert.Equal(2, canExecuteChangeCalled);
+      }
+
+      [Fact]
+      public void RedoCanExecuteChangeFiresCorrectlyForRedoOperation() {
+         var canExecuteChangeCalled = 0;
+         history.Redo.CanExecuteChanged += (sender, e) => canExecuteChangeCalled++;
+
+         history.CurrentChange.Add(3);
+         Assert.Equal(0, canExecuteChangeCalled);
+
+         history.Undo.Execute(null);
+         Assert.Equal(1, canExecuteChangeCalled);
+
+         history.Redo.Execute(null);
+         Assert.Equal(2, canExecuteChangeCalled);
+      }
+
+      [Fact]
+      public void RedoCanExecuteChangeFiresCorrectlyForNewChange() {
+         var canExecuteChangeCalled = 0;
+         history.Redo.CanExecuteChanged += (sender, e) => canExecuteChangeCalled++;
+
+         history.CurrentChange.Add(3);
+         Assert.Equal(0, canExecuteChangeCalled);
+
+         history.Undo.Execute(null);
+         Assert.Equal(1, canExecuteChangeCalled);
+
+         history.CurrentChange.Add(4); // adding another change clears the redo stack
+         Assert.Equal(2, canExecuteChangeCalled);
+      }
+   }
+}
