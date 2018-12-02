242e7199b9b129af15ac68d3b8600f67cc837eb7
Benjamin Popp <haven1433@gmail.com>
2018-12-02 12:59:18 -0600

    get version string from attributes, so that I can update it in only one place.

--- Gen3Hex/View/AboutWindow.xaml.cs
+++ Gen3Hex/View/AboutWindow.xaml.cs
@@ -1,9 +1,16 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
 using System.Windows.Navigation;
 
 namespace HavenSoft.Gen3Hex.View {
    public partial class AboutWindow {
-      public AboutWindow() => InitializeComponent();
+      public AboutWindow() {
+         InitializeComponent();
+
+         var assembly = Assembly.GetExecutingAssembly();
+         var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+         Version.Text = $"Version {fvi.FileMajorPart}.{fvi.FileMinorPart}.{fvi.FileBuildPart}";
+      }
 
       private void Navigate(object sender, RequestNavigateEventArgs e) {
          Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
