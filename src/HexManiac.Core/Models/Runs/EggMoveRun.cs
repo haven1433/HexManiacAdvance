0efa5a4c59eaf687c8764f11bac87b40bee4351e
Benjamin Popp <haven1433@gmail.com>
2019-05-14 21:18:24 -0500

    Autosearch for egg move stream
    Altair messes this up, because in that fangame, the egg moves were edited but the limiter wasn't. So the two lengths don't match, and the egg-moves were filled with "00 00".
    
    To support other ROMs with this kind of problem, accept early exits from the format searcher.

--- src/HexManiac.Core/Models/Runs/EggMoveRun.cs
+++ src/HexManiac.Core/Models/Runs/EggMoveRun.cs
@@ -7,6 +7,8 @@ using System.Linq;
 namespace HavenSoft.HexManiac.Core.Models.Runs {
    public class EggMoveRun : IFormattedRun {
       public const int MagicNumber = 0x4E20; // anything above this number is a pokemon, anything below it is a move
+      public const string PokemonNameTable = "pokenames";
+      public const string MoveNamesTable = "movenames";
       private readonly IDataModel model;
 
       public int Start { get; }
@@ -26,13 +28,58 @@ namespace HavenSoft.HexManiac.Core.Models.Runs {
          }
       }
 
+      public static bool TrySearch(IDataModel data, ModelDelta token, out EggMoveRun eggmoves) {
+         eggmoves = default;
+         var pokenames = data.GetNextRun(data.GetAddressFromAnchor(token, -1, PokemonNameTable)) as ArrayRun;
+         var movenames = data.GetNextRun(data.GetAddressFromAnchor(token, -1, MoveNamesTable)) as ArrayRun;
+         if (pokenames == null || movenames == null) return false;
+
+         for (var run = data.GetNextRun(0); run.Start < int.MaxValue; run = data.GetNextRun(run.Start + run.Length)) {
+            if (run is ArrayRun || run is PCSRun || run.PointerSources == null) continue;
+
+            // verify expected pointers to this
+            if (run.PointerSources.Count != 2) continue;
+
+            // verify limiter
+            var length = data.ReadMultiByteValue(run.PointerSources[1] - 4, 4);
+
+            // we just read the 'length' from basically a random byte... verify that it could make sense as a length
+            if (length < 0) continue;
+            if (run.Start + length * 2 + 3 < 0) continue;
+            if (run.Start + length * 2 + 3 > data.Count) continue;
+            var endValue = data.ReadMultiByteValue(run.Start + length * 2 + 2, 2);
+            if (endValue != 0xFFFF) continue;
+
+            // verify content
+            bool possibleMatch = true;
+            for (int i = 0; i < length - 2; i++) {
+               var value = data.ReadMultiByteValue(run.Start + i * 2, 2);
+               if (value == 0xFFFF) break; // early exit, the data was edited, but that's ok. Everything still matches up.
+               if (value >= MagicNumber) {
+                  value -= MagicNumber;
+                  if (value < pokenames.ElementCount) continue;
+               }
+               if (value < movenames.ElementCount) continue;
+               possibleMatch = false;
+               break;
+            }
+            if (!possibleMatch) continue;
+
+            // content is correct, length is correct: this is it!
+            eggmoves = new EggMoveRun(data, run.Start);
+            return true;
+         }
+
+         return false;
+      }
+
       private IReadOnlyList<string> cachedPokenames, cachedMovenames;
       private int lastFormatRequest = int.MinValue;
       public IDataFormat CreateDataFormat(IDataModel data, int dataIndex) {
          Debug.Assert(data == model);
          if (dataIndex != lastFormatRequest + 1) {
-            cachedPokenames = ArrayRunEnumSegment.GetOptions(model, "pokenames") ?? new List<string>();
-            cachedMovenames = ArrayRunEnumSegment.GetOptions(model, "movenames") ?? new List<string>();
+            cachedPokenames = ArrayRunEnumSegment.GetOptions(model, PokemonNameTable) ?? new List<string>();
+            cachedMovenames = ArrayRunEnumSegment.GetOptions(model, MoveNamesTable) ?? new List<string>();
          }
          lastFormatRequest = dataIndex;
 
