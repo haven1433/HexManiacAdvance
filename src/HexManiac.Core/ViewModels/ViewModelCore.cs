7f2655655ebff7973efc0474b859b360b59241ee
Benjamin Popp <haven1433@gmail.com>
2019-07-17 22:02:15 -0500

    Add Scripting to Code Tool
    Being able to dissassemble ARM code is only half of the code that hackers want to see. They also need to be able to read/write pokescript, often done via tools like XSE. Eventually I can be more intelligent with scripts, but for now a simple decoder is plenty.

--- src/HexManiac.Core/ViewModels/ViewModelCore.cs
+++ src/HexManiac.Core/ViewModels/ViewModelCore.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace HavenSoft.HexManiac.Core.ViewModels {
@@ -26,6 +28,31 @@ namespace HavenSoft.HexManiac.Core.ViewModels {
       protected bool TryUpdate<T>(ref T backingField, T newValue, [CallerMemberName]string propertyName = null) where T : IEquatable<T> {
          if (backingField == null && newValue == null) return false;
          if (backingField != null && backingField.Equals(newValue)) return false;
+         var oldValue = backingField;
+         backingField = newValue;
+         NotifyPropertyChanged(oldValue, propertyName);
+         return true;
+      }
+
+      protected bool TryUpdateEnum<T>(ref T backingField, T newValue, [CallerMemberName]string propertyName = null) where T : Enum {
+         if (backingField.Equals(newValue)) return false;
+         var oldValue = backingField;
+         backingField = newValue;
+         NotifyPropertyChanged(oldValue, propertyName);
+         return true;
+      }
+
+      protected bool TryUpdateSequence<T, U>(ref T backingField, T newValue, [CallerMemberName]string propertyName = null) where T : IEnumerable<U> where U : IEquatable<U> {
+         if (backingField == null && newValue == null) return false;
+         if (backingField != null && backingField.Count() == newValue.Count()) {
+            bool allMatch = true;
+            foreach (var pair in backingField.Zip(newValue, (a, b) => (a, b))) {
+               if (pair.a.Equals(pair.b)) continue;
+               allMatch = false;
+               break;
+            }
+            if (allMatch) return false;
+         }
          var oldValue = backingField;
          backingField = newValue;
          NotifyPropertyChanged(oldValue, propertyName);
