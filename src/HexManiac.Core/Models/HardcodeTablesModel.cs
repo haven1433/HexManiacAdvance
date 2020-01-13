7258ddaf8eecff787602699a0197be6238ce9efc
Benjamin Popp <haven1433@gmail.com>
2020-01-12 21:13:22 -0600

    Condense known tables into a reference file
    Other things should probably be in this reference file too, such as the headers, streams, and default lists. But this is a good start and should make it more expandable in the future.

--- src/HexManiac.Core/Models/HardcodeTablesModel.cs
+++ src/HexManiac.Core/Models/HardcodeTablesModel.cs
@@ -1,6 +1,7 @@
 ﻿using HavenSoft.HexManiac.Core.Models.Runs;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using static HavenSoft.HexManiac.Core.Models.AutoSearchModel;
 
@@ -63,9 +64,7 @@ namespace HavenSoft.HexManiac.Core.Models {
          if (gamesToDecode.Contains(gameCode)) {
             AddDefaultLists();
             DecodeHeader();
-            DecodeNameArrays();
-            DecodeDataArrays();
-            DecodeDexArrays();
+            DecodeTablesFromReference();
             DecodeStreams();
          }
 
@@ -82,297 +81,24 @@ namespace HavenSoft.HexManiac.Core.Models {
          }
       }
 
-      private void DecodeNameArrays() {
-         // pokenames
-         int source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: case Emerald: case FireRed1_1: case LeafGreen1_1: source = 0x000144; break;
-            case Ruby: case Sapphire: case Ruby1_1: case Sapphire1_1: source = 0x00FA58; break;
+      private static readonly string[] referenceOrder = new string[] { "name", Ruby, Sapphire, Ruby1_1, Sapphire1_1, FireRed, LeafGreen, FireRed1_1, LeafGreen1_1, Emerald, "format" };
+      private void DecodeTablesFromReference() {
+         var gameIndex = referenceOrder.IndexOf(gameCode);
+         if (gameIndex == -1) return;
+         if (!File.Exists("resources/tableReference.txt")) return;
+         var lines = File.ReadAllLines("resources/tableReference.txt");
+         foreach (var line in lines) {
+            var row = line.Trim();
+            if (row.StartsWith("//")) continue;
+            var segments = row.Split(",");
+            if (segments.Length != referenceOrder.Length) continue;
+            var addressHex = segments[gameIndex].Trim();
+            if (addressHex == string.Empty) continue;
+            if (!int.TryParse(addressHex, NumberStyles.HexNumber, CultureInfo.CurrentCulture, out int address)) continue;
+            var name = segments[0].Trim();
+            var format = segments.Last().Trim();
+            AddTable(address, name, format);
          }
-         AddTable(source, EggMoveRun.PokemonNameTable, "[name\"\"11]");
-
-         // movenames
-         source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: case Emerald: case FireRed1_1: case LeafGreen1_1: source = 0x000148; break;
-            case Ruby: case Sapphire: case Ruby1_1: case Sapphire1_1: source = 0x02E18C; break;
-         }
-         AddTable(source, EggMoveRun.MoveNamesTable, "[name\"\"13]");
-
-         // abilitynames
-         source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: case Emerald: case FireRed1_1: case LeafGreen1_1: source = 0x0001C0; break;
-            case Ruby: case Sapphire: source = 0x09FE64; break;
-            case Ruby1_1: case Sapphire1_1: source = 0x09FE84; break;
-         }
-         AddTable(source, "abilitynames", "[name\"\"13]");
-
-         // trainerclassnames
-         source = 0;
-         switch (gameCode) {
-            case FireRed: source = 0x0D80A0; break;
-            case LeafGreen: source = 0x0D8074; break;
-            case FireRed1_1: source = 0x0D80B4; break;
-            case LeafGreen1_1: source = 0x0D8088; break;
-            case Emerald: source = 0x0183B4; break;
-            case Ruby: case Sapphire: source = 0x1217BC; break;
-            case Ruby1_1: case Sapphire1_1: source = 0x0F70A8; break;
-         }
-         AddTable(source, "trainerclassnames", "[name\"\"13]");
-
-         // types
-         source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: source = 0x0309C8; break;
-            case FireRed1_1: case LeafGreen1_1: source = 0x0309DC; break;
-            case Emerald: source = 0x0166F4; break;
-            case Ruby: case Sapphire: case Ruby1_1: case Sapphire1_1: source = 0x02E3A8; break;
-         }
-         AddTable(source, "types", "^[name\"\"7]");
-
-         // abilitydescriptions
-         source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: case Emerald: case FireRed1_1: case LeafGreen1_1: source = 0x0001C4; break;
-            case Ruby: case Sapphire: source = 0x09FE68; break;
-            case Ruby1_1: case Sapphire1_1: source = 0x09FE88; break;
-         }
-         AddTable(source, "abilitydescriptions", $"[description<{PCSRun.SharedFormatString}>]abilitynames");
-
-         // movedescriptions
-         source = 0;
-         switch (gameCode) {
-            case Ruby: case Sapphire: source = 0x0A0494; break;
-            case Ruby1_1: case Sapphire1_1: source = 0x0A04B4; break;
-            case FireRed: source = 0x0E5440; break;
-            case LeafGreen: source = 0x0E5418; break;
-            case FireRed1_1: source = 0x0E5454; break;
-            case LeafGreen1_1: source = 0x0E542C; break;
-            case Emerald: source = 0x1C3EFC; break;
-         }
-         AddTable(source, MoveDescriptionsName, $"[description<{PCSRun.SharedFormatString}>]{EggMoveRun.MoveNamesTable}-1");
-
-         // multichoice
-         source = 0;
-         switch (gameCode) {
-            case Ruby: case Sapphire: source = 0x0B50A0; break;
-            case Ruby1_1: case Sapphire1_1: source = 0x0B50C0; break;
-            case FireRed: source = 0x09CB58; break;
-            case LeafGreen: source = 0x09CB2C; break;
-            case FireRed1_1: source = 0x09CB6C; break;
-            case LeafGreen1_1: source = 0x09CB40; break;
-            case Emerald: source = 0x0E1FB8; break;
-         }
-         AddTable(source, MultichoiceTableName, $"[options<[text<\"\"> unused::]/count> count::]");
-      }
-
-      private void DecodeDataArrays() {
-         // pokestats
-         int source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: case Emerald: case FireRed1_1: case LeafGreen1_1: source = 0x0001BC; break;
-            case Ruby: case Sapphire: case Ruby1_1: case Sapphire1_1: source = 0x010B64; break;
-         }
-         var format = $"[hp. attack. def. speed. spatk. spdef. type1.types type2.types catchRate. baseExp. evs: item1:{ItemsTableName} item2:{ItemsTableName} genderratio. steps2hatch. basehappiness. growthrate. egg1. egg2. ability1.abilitynames ability2.abilitynames runrate. unknown. padding:]{EggMoveRun.PokemonNameTable}";
-         AddTable(source, "pokestats", format);
-
-         // evolutions
-         source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: source = 0x42F6C; break;
-            case FireRed1_1: case LeafGreen1_1: source = 0x42F80; break;
-            case Ruby: case Sapphire: case Ruby1_1: case Sapphire1_1: source = 0x3F534; break;
-            case Emerald: source = 0x6D140; break;
-         }
-         AddTable(source, EvolutionTableName, $"[[method:{EvolutionMethodListName} arg: species:{EggMoveRun.PokemonNameTable} unused:]5]{EggMoveRun.PokemonNameTable}");
-
-         // items
-         source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: case Emerald: case FireRed1_1: case LeafGreen1_1: source = 0x0001C8; break;
-            case Ruby: case Sapphire: source = 0x0A98F0; break;
-            case Ruby1_1: case Sapphire1_1: source = 0x0A9910; break;
-         }
-         format = $"[name\"\"14 index: price: holdeffect. param. description<{PCSRun.SharedFormatString}> keyitemvalue. bagkeyitem. pocket. type. fieldeffect<> battleusage:: battleeffect<> battleextra::]";
-         AddTable(source, ItemsTableName, format);
-
-         // movedata
-         source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: case Emerald: case FireRed1_1: case LeafGreen1_1: source = 0x0001CC; break;
-            case Ruby: case Sapphire: case Ruby1_1: case Sapphire1_1: source = 0x00CA54; break;
-         }
-         format = $"[effect.{MoveEffectListName} power. type.types accuracy. pp. effectAccuracy. target|b[]{MoveTargetListName} priority. info|b[]{MoveInfoListName} unused. unused:]{EggMoveRun.MoveNamesTable}";
-         AddTable(source, "movedata", format);
-
-         // lvlmoves
-         source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: source = 0x03EA7C; break;
-            case FireRed1_1: case LeafGreen1_1: source = 0x03EA90; break;
-            case Emerald: source = 0x06930C; break;
-            case Ruby: case Sapphire: case Ruby1_1: case Sapphire1_1: source = 0x03B7BC; break;
-         }
-         using (ModelCacheScope.CreateScope(this)) { // cares about pokenames and movenames
-            AddTable(source, LevelMovesTableName, $"[movesFromLevel<{PLMRun.SharedFormatString}>]{EggMoveRun.PokemonNameTable}");
-         }
-
-         // tutormoves / tutorcompatibility
-         source = 0;
-         if (gameCode != Ruby && gameCode != Sapphire && gameCode!= Ruby1_1 && gameCode!= Sapphire1_1) {
-            switch (gameCode) {
-               case FireRed: source = 0x120BE4; break;
-               case LeafGreen: source = 0x120BBC; break;
-               case FireRed1_1: source = 0x120C5C; break;
-               case LeafGreen1_1: source = 0x120C34; break;
-               case Emerald: source = 0x1B236C; break;
-            }
-            format = $"[move:{EggMoveRun.MoveNamesTable}]" + (gameCode == Emerald ? 30 : 15);
-            AddTable(source, MoveTutors, format);
-            source += gameCode == Emerald ? 0x24 : 0x4C;
-            format = $"[moves|b[]{MoveTutors}]{EggMoveRun.PokemonNameTable}";
-            AddTable(source, TutorCompatibility, format);
-         }
-
-         // tmmoves
-         source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: source = 0x125A60; break;
-            case FireRed1_1: source = 0x125B04; break;
-            case LeafGreen1_1: source = 0x125ADC; break;
-            case Emerald: source = 0x1B6D10; break;
-            case Ruby: case Sapphire: source = 0x06F038; break;
-            case Ruby1_1: source = 0x06F058; break;
-            case Sapphire1_1: source = 0x06F05C; break;
-         }
-         if (source != 0) source = GetNextRun(source).Start;
-         AddTable(source, TmMoves, $"[move:{EggMoveRun.MoveNamesTable}]58");
-
-         // tmcompatibility
-         source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: source = 0x043C68; break;
-            case FireRed1_1: case LeafGreen1_1: source = 0x043C7C; break;
-            case Emerald: source = 0x06E048; break;
-            case Ruby: case Sapphire: source = 0x0403B0; break;
-            case Ruby1_1: case Sapphire1_1: source = 0x0403D0; break;
-         }
-         AddTable(source, TmCompatibility, $"[moves|b[]{TmMoves}]{EggMoveRun.PokemonNameTable}");
-
-         // hmmoves
-         source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: source = 0x0441DC; break;
-            case FireRed1_1: case LeafGreen1_1: source = 0x0441F0; break;
-            case Emerald: source = 0x06E828; break;
-            case Ruby: case Sapphire: source = 0x040A24; break;
-            case Ruby1_1: case Sapphire1_1: source = 0x040A44; break;
-         }
-         AddTable(source, HmMoves, $"[move:{EggMoveRun.MoveNamesTable}]8");
-
-         // itemimages
-         source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: source = 0x098970; break;
-            case FireRed1_1: source = 0x0989B0; break;
-            case LeafGreen1_1: source = 0x098984; break;
-            case Emerald: source = 0x1B0034; break;
-            case Ruby: case Sapphire: source = -1; break;
-         }
-         if (source != 0) source = GetNextRun(source).Start;
-         AddTable(source, "itemimages", $"[image<> palette<>]{ItemsTableName}");
-
-         // trainer teams
-         source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: source = 0x00FC00; break;
-            case FireRed1_1: case LeafGreen1_1: source = 0x00FC14; break;
-            case Emerald: source = 0x03587C; break;
-            case Ruby: case Sapphire: case Ruby1_1: case Sapphire1_1: source = 0x00D890; break;
-         }
-         AddTable(source, TrainerTableName, $"[structType.4 class.trainerclassnames introMusic. sprite. name\"\"12 item1:{ItemsTableName} item2:{ItemsTableName} item3:{ItemsTableName} item4:{ItemsTableName} doubleBattle:: ai:: pokemonCount:: pokemon<`tpt`>]");
-
-         // decorations
-         source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: case Emerald: case FireRed1_1: case LeafGreen1_1: source = 0x00014C; break;
-            case Ruby: case Sapphire: source = 0x0B3AC8; break;
-            case Ruby1_1: case Sapphire1_1: source = 0x0B3AE8; break;
-         }
-         AddTable(source, DecorationsTableName, $"[id. name\"\"16 permission.{DecorationsPermissionListName} shape.{DecorationsShapeListName} category.{DecorationsCategoryListName} price:: description<\"\"> graphics<>]");
-
-         // wild pokemon
-         source = Find("0348048009E00000FFFF0000");
-         string table(int length) => $"<[rate:: list<[low. high. species:pokenames]{length}>]1>";
-         using (ModelCacheScope.CreateScope(this)) { // cares about pokenames
-            AddTable(source, WildTableName, $"[bank. map. unused: grass{table(12)} surf{table(5)} tree{table(5)} fish{table(10)}]");
-         }
-
-         // specials
-         source = 0;
-         switch (gameCode) {
-            case FireRed: case LeafGreen: source = 0x069F18; break;
-            case FireRed1_1: case LeafGreen1_1: source = 0x069F2C; break;
-            case Ruby: source = 0x0658B4; break;
-            case Sapphire: source = 0x0658B8; break;
-            case Ruby1_1: source = 0x0658D4; break;
-            case Sapphire1_1: source = 0x0658D8; break;
-            case Emerald: source = 0x099314; break;
-         }
-         AddTable(source, SpecialsTable, "[code<>]");
-      }
-
-      private void DecodeDexArrays() {
-         // regional dex
-         int source = 0;
-         switch (gameCode) {
-            case Ruby: case Sapphire: case Ruby1_1: case Sapphire1_1: source = 0x03F7F0; break;
-            case FireRed: case LeafGreen: source = 0x0431F0; break;
-            case FireRed1_1: case LeafGreen1_1: source = 0x043204; break;
-            case Emerald: source = 0x06D3FC; break;
-         }
-         AddTable(source, RegionalDexTableName, "[index:]pokenames-1");
-
-         // national dex
-         source = 0;
-         switch (gameCode) {
-            case Ruby: case Sapphire: case Ruby1_1: case Sapphire1_1: source = 0x03F83C; break;
-            case FireRed: case LeafGreen: source = 0x04323C; break;
-            case FireRed1_1: case LeafGreen1_1: source = 0x043250; break;
-            case Emerald: source = 0x06D448; break;
-         }
-         AddTable(source, NationalDexTableName, "[index:]pokenames-1");
-
-         // hoenn-to-national conversion
-         // hoenn[treecko]  =   1, national[treecko]  = 252, HoeennToNationalDex[ 1]= 252
-         // hoenn[bulbasaur]= 203, national[bulbasaur]=   1, HoennToNationalDex[203]=   1
-         // -> this table's values can be determined automatically based on the first two
-         source = 0;
-         switch (gameCode) {
-            case Ruby: case Sapphire: case Ruby1_1: case Sapphire1_1: source = 0x03F888; break;
-            case FireRed: case LeafGreen: source = 0x043288; break;
-            case FireRed1_1: case LeafGreen1_1: source = 0x04329C; break;
-            case Emerald: source = 0x06D494; break;
-         }
-         AddTable(source, ConversionDexTableName, "[index:]pokenames-1");
-
-         // dex info
-         source = 0;
-         switch (gameCode) {
-            case Ruby: case Sapphire: source = 0x08F508; break;
-            case Ruby1_1: case Sapphire1_1: source = 0x08F528; break;
-            case FireRed: source = 0x088E34; break;
-            case LeafGreen: source = 0x088E30; break;
-            case FireRed1_1: source = 0x088E48; break;
-            case LeafGreen1_1: source = 0x088E1C; break;
-            case Emerald: source = 0x0BFA20; break;
-         }
-         // height is in decimeters
-         // weight is in hectograms
-         var format = "[species\"\"12 height: weight: description1<\"\"> description2<\"\"> unused: pokemonScale: pokemonOffset: trainerScale: trainerOffset: unused:]";
-         if (gameCode == Emerald) format = "[species\"\"12 height: weight: description<\"\"> unused: pokemonScale: pokemonOffset: trainerScale: trainerOffset: unused:]";
-         AddTable(source, DexInfoTableName, format);
       }
 
       private void DecodeStreams() {
