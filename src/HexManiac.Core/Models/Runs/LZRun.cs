f524de8262ed8c89eec8a5a3c3e415c0a1c25075
Benjamin Popp <haven1433@gmail.com>
2020-01-21 22:25:58 -0600

    Add initial image tests
    none of these pass yet

---
+++ src/HexManiac.Core/Models/Runs/LZRun.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HavenSoft.HexManiac.Core.ViewModels.DataFormats;
+
+namespace HavenSoft.HexManiac.Core.Models.Runs {
+   public class LZRun : BaseRun {
+      public override int Length { get; }
+
+      public override string FormatString { get; }
+
+      public LZRun(IReadOnlyList<byte> data, int start, IReadOnlyList<int> sources = null) : base(start, sources) {
+      }
+
+      // ported from another project
+      public static byte[] UncompressLZ(byte[] memory, int offset) {
+         // all LZ compressed data starts with 0x10
+         if (memory[offset] != 0x10) return null;
+         int length = (memory[offset + 3] << 16) | (memory[offset + 2] << 8) | (memory[offset + 1] << 0);
+         var uncompressed = new List<byte>();
+         offset += 4;
+
+         while (true) {
+            // always start with a bitfield
+            // it encodes the next 8 steps
+            // "1" means its a runlength dictionary compression
+            //     and the dictionary is the most recent decompressed data
+            // "0" means its decompressed
+            // this makes a fully compressed data stream only 12.5% longer than it was when it started (at worst).
+            var bitField = memory[offset++];
+            var bits = new[] {
+               (bitField & 0x80) != 0,
+               (bitField & 0x40) != 0,
+               (bitField & 0x20) != 0,
+               (bitField & 0x10) != 0,
+               (bitField & 0x08) != 0,
+               (bitField & 0x04) != 0,
+               (bitField & 0x02) != 0,
+               (bitField & 0x01) != 0,
+            };
+
+            foreach (var bit in bits) {
+               if (bit) {
+                  // the next two bytes explain the dictionary position/length of the next set of bytes.
+                  // aaaa bbbb . bbbb bbbb
+
+                  // aaaa : the runlength of the dictionary encoding. Never less than 3, so the run
+                  //        is encoded as 3 smaller (to allow for slightly larger runs).
+                  //        possible final values: 3-18
+                  // bbbb bbbb bbbb : how far from the end of the stream to start reading for the run.
+                  //                  never 0, so the value is encoded as 1 smaller to allow for slightly
+                  //                  longer backtracks.
+                  //                  possible final values: 1-4096
+
+                  if (offset >= memory.Length) return null;
+                  var byte1 = memory[offset++];
+                  var runLength = (byte1 >> 4) + 3;
+                  var runOffset_upper = (byte1 & 0xF) << 8;
+                  var runOffset_lower = memory[offset++];
+                  var runOffset = (runOffset_lower | runOffset_upper) + 1;
+                  if (runOffset > uncompressed.Count) return null;
+                  foreach (var i in Enumerable.Range(0, runLength)) {
+                     uncompressed.Add(uncompressed[uncompressed.Count - runOffset]);
+                     if (uncompressed.Count == length) return uncompressed.ToArray();
+                  }
+               } else {
+                  uncompressed.Add(memory[offset++]);
+                  if (uncompressed.Count == length) return uncompressed.ToArray();
+               }
+            }
+         }
+      }
+
+      public static bool IsCompressedLzData(byte[] data, int start) => false;
+
+      public static byte[] Decompress(byte[] data, int start) => null;
+
+      public override IDataFormat CreateDataFormat(IDataModel data, int index) {
+         throw new NotImplementedException();
+      }
+
+      protected override BaseRun Clone(IReadOnlyList<int> newPointerSources) {
+         throw new NotImplementedException();
+      }
+   }
+}
---
+++ src/HexManiac.Tests/ImageTests.cs
@@ -0,0 +1,48 @@
+﻿using HavenSoft.HexManiac.Core.Models.Runs;
+using Xunit;
+
+namespace HavenSoft.HexManiac.Tests {
+   public class ImageTests : BaseViewModelTestClass {
+      [Fact]
+      public void ValidHeaderIsRecognized() {
+         var data = new byte[] { 0x10, 8, 0, 0, 0b000000, 0, 0, 0, 0, 0, 0, 0, 0 };
+         Assert.True(LZRun.IsCompressedLzData(data, 0));
+      }
+
+      [Theory]
+      [InlineData(new byte[] { 0x00, 8, 0, 0, 0b000000, 0, 0, 0, 0, 0, 0, 0, 0 })] // first byte must be 0x10
+      [InlineData(new byte[] { 0x10, 8, 1, 0, 0b000000, 0, 0, 0, 0, 0, 0, 0, 0 })] // must contain enough bytes to read the whole format
+      [InlineData(new byte[] { 0x10, 6, 0, 0, 0b100000, 0, 0, 0, 0, 0, 0, 0, 0 })] // any unused bits at the end of the 'compression' byte must be 0
+      [InlineData(new byte[] { 0x10, 7, 0, 0, 0b000001, 0, 0, 0, 0, 0, 0, 0, 0 })] // first bit of the first 'compression' byte must be 0, since compression looks back
+      [InlineData(new byte[] { 0x10, 7, 0, 0, 0b000010, 0, 0, 4, 0, 0, 0, 0, 0 })] // compressed bytes cannot start further back than the start of the stream
+      public void ValidHeaderIsInvalid(byte[] data) {
+         Assert.False(LZRun.IsCompressedLzData(data, 0));
+      }
+
+      [Theory]
+      [InlineData(
+         new byte[] { 0x10, 8, 0, 0, 0b000000, 0, 0, 0, 0, 0, 0, 0, 0 },
+         new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 }
+      )]
+      public void Decompress(byte[] compressed, byte[] uncompressed) {
+         var result = LZRun.Decompress(compressed, 0);
+         Assert.Equal(uncompressed, result);
+      }
+
+      [Fact]
+      public void RunHasExpectedLength() {
+         var data = new byte[] { 0x10, 8, 0, 0, 0b000000, 0, 0, 0, 0, 0, 0, 0, 0 };
+         var run = new LZRun(data, 0);
+         Assert.Equal(13, run.Length);
+      }
+
+      // TODO validate the dataformats that come back from the LZRun: length 1 for header, 3 for length, 1 for bitfield, 1 for raw, and 2 for compressed
+      // TODO if an LZRun compressed segment is edited such that the segment becomes longer, the overall LZRun becomes shorter. Fix up the end as needed, clearing excess bytes.
+      // TODO if an LZRun compressed segment is edited such that the segment becomes shorter, the overall LZRun becomes longer. Append to the end as needed, adding extra '00' bytes
+      // TODO verify that an LZRun 1-byte header cannot be edited
+      // TODO if an LZRun decompressed length is edited, fix up end as needed
+      // TODO if an LZRun bitfield segment is edited, reinterpret everything after and fixup end as needed
+      // TODO if an LZRun decompressed segment is edited, we're fine
+      // TODO if an LZRun has a length requirement that isn't met (example, the image is known to be 32x32 in size), error if the length is changed
+   }
+}
