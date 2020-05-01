8aae2250f13562c09556da0912b7e5c6ba07efed
Benjamin Popp <haven1433@gmail.com>
2020-05-01 08:16:32 -0500

    Version Bump

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -6,5 +6,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.3.4.0")]
-[assembly: AssemblyFileVersion("0.3.4.0")]
+[assembly: AssemblyVersion("0.3.4.1")]
+[assembly: AssemblyFileVersion("0.3.4.1")]
