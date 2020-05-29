6a413c48bf3167a81dca5dc63c1418f035f22b04
Benjamin Popp <haven1433@gmail.com>
2020-05-29 18:56:33 -0500

    Add new test class

---
+++ src/HexManiac.Tests/PaletteCollectionTests.cs
@@ -0,0 +1,14 @@
+﻿using HavenSoft.HexManiac.Core.Models;
+using HavenSoft.HexManiac.Core.ViewModels;
+using HavenSoft.HexManiac.Core.ViewModels.Tools;
+using Xunit;
+
+namespace HavenSoft.HexManiac.Tests {
+   public class PaletteCollectionTests : BaseViewModelTestClass {
+      private readonly StubFileSystem fileSystem = new StubFileSystem();
+
+      public PaletteCollectionTests() {
+         ViewPort.Edit("^pal`ucp4`");
+      }
+   }
+}
