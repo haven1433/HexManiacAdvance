1f03e2bc7e5c105594ed90e24a8df633c7084060
Benjamin Popp <haven1433@gmail.com>
2020-05-29 18:57:53 -0500

    Add tests for copy/paste

--- src/HexManiac.Tests/PaletteCollectionTests.cs
+++ src/HexManiac.Tests/PaletteCollectionTests.cs
@@ -10,5 +10,26 @@ namespace HavenSoft.HexManiac.Tests {
       public PaletteCollectionTests() {
          ViewPort.Edit("^pal`ucp4`");
       }
+
+      [Fact]
+      public void CanCopy() {
+         var collection = ViewPort.Tools.SpriteTool.Colors;
+
+         collection.SelectionStart = 0;
+         collection.Copy.Execute(fileSystem);
+
+         Assert.Equal("0:0:0", fileSystem.CopyText);
+      }
+
+      [Fact]
+      public void CanPaste() {
+         var collection = ViewPort.Tools.SpriteTool.Colors;
+
+         collection.SelectionStart = 0;
+         fileSystem.CopyText = "1:1:1";
+         collection.Paste.Execute(fileSystem);
+
+         Assert.Equal(0b0_00001_00001_00001, Model.ReadMultiByteValue(0, 2));
+      }
    }
 }
