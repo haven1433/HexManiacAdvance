a38b61675d4287d50bd261423e1a45a571436777
Benjamin Popp <haven1433@gmail.com>
2020-07-14 22:58:50 -0500

    Remove some restrictions from the DexReorderTab
    expanding it to support multiple pokemon in the same pokedex slot. Example: alolan forms.

--- src/HexManiac.Core/ViewModels/DexReorderTab.cs
+++ src/HexManiac.Core/ViewModels/DexReorderTab.cs
@@ -6,7 +6,6 @@ using HavenSoft.HexManiac.Core.ViewModels.Tools;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows.Input;
@@ -116,31 +115,38 @@ namespace HavenSoft.HexManiac.Core.ViewModels {
          var indexName = dexOrder.ElementContent[0].Name;
 
          var elements = new SortablePokemon[dexInfo.ElementCount - 1];
-         var unusedIndeces = new Dictionary<int, int>();
+         var usedIndices = new Dictionary<int, int>();
          for (int i = 0; i < elementCount; i++) {
             var dexIndex = dexOrderTable[i].GetValue(indexName);
             if (dexIndex >= dexInfo.ElementCount) {
                if (dexIndex > elementCount) {
                   Debug.Fail($"Dex Reorder Warning: pokemon {i} is set to pokedex slot {dexIndex - 1} which is more than the number of pokemon!");
                }
-               if (!unusedIndeces.ContainsKey(dexIndex)) {
-                  unusedIndeces[dexIndex] = 0;
+               if (!usedIndices.ContainsKey(dexIndex)) {
+                  usedIndices[dexIndex] = 0;
                }
-               unusedIndeces[dexIndex] += 1;
+               usedIndices[dexIndex] += 1;
                continue;
             }
-            if (elements[dexIndex - 1] != null) Debug.Fail($"Dex Reorder Warning: pokedex slot {dexIndex - 1} is set more than once! See pokemon {i}.");
-            elements[dexIndex - 1] = new SortablePokemon(model, i + 1);
+            if (dexIndex == 0 || dexIndex > elements.Length) continue; // out of range: we don't reall care about this value.
+            if (elements[dexIndex - 1] != null) {
+               // multiple pokemon set to the same slot. Example: Rattata and Alolan Rattata
+               elements[dexIndex - 1].AddSource(i + 1);
+            } else {
+               elements[dexIndex - 1] = new SortablePokemon(model, i + 1);
+            }
          }
          for (int i = dexInfo.ElementCount; i < elementCount; i++) {
-            if (!unusedIndeces.ContainsKey(i)) {
-               Debug.Fail($"Dex Reorder Warning: pokedex slot {i} is not used!");
-            } else if (unusedIndeces[i] > 1) {
+            if (!usedIndices.ContainsKey(i)) {
+               // TODO I still care about this, but I probably want to put it in a warnings tab, not here.
+               // It might cause problems for the hacker, but not for the program.
+               // Debug.Fail($"Dex Reorder Warning: pokedex slot {i} is not used!");
+            } else if (usedIndices[i] > 1) {
                Debug.Fail($"Dex Reorder Warning: pokedex slot {i} is used more than once!");
             }
          }
          for (int i = 0; i < elements.Length; i++) {
-            Debug.Assert(elements[i] != null, $"Dex Reorder warning: pokedex slot {i + 1} is empty!");
+            if (elements[i] == null) elements[i] = new SortablePokemon(model, 0); // unused pokedex slot
             Elements.Add(elements[i]);
             elements[i].MatchToFilter(filter);
          }
@@ -207,8 +213,7 @@ namespace HavenSoft.HexManiac.Core.ViewModels {
          }
 
          // maps from canonical order to new desired dex order
-         Debug.Assert(Elements.All(item => item != null), "Dex Reorder only works if there are no empty pokedex slots!");
-         var newOrder = Invert(Elements.Select(element => element.CanonicalIndex).ToList(), oldOrder);
+         var newOrder = Invert(Elements, oldOrder);
 
          var oldDexInfo = new byte[dexInfo.Length];
          Array.Copy(model.RawData, dexInfo.Start, oldDexInfo, 0, dexInfo.Length);
@@ -221,11 +226,15 @@ namespace HavenSoft.HexManiac.Core.ViewModels {
             if (isNational) {
                // we only have to update the dex info if this tab is editing the nationaldex.
                var originalIndex = i;
-               var newIndex = newOrder[oldOrder.IndexOf(i)];
-               if (newIndex != originalIndex) {
-                  // update data
-                  for (int j = 0; j < dexInfo.ElementLength; j++) {
-                     token.ChangeData(model, dexInfo.Start + dexInfo.ElementLength * newIndex + j, oldDexInfo[dexInfo.ElementLength * originalIndex + j]);
+               var previousIndices = Enumerable.Range(0, oldOrder.Count).Where(j => oldOrder[j] == i).ToList();
+               var newIndices = previousIndices.Select(j => newOrder[j]).Distinct().ToList();
+               // var newIndex = newOrder[oldOrder.IndexOf(i)];
+               foreach (var newIndex in newIndices) {
+                  if (newIndex != originalIndex) {
+                     // update data
+                     for (int j = 0; j < dexInfo.ElementLength; j++) {
+                        token.ChangeData(model, dexInfo.Start + dexInfo.ElementLength * newIndex + j, oldDexInfo[dexInfo.ElementLength * originalIndex + j]);
+                     }
                   }
                }
             }
@@ -242,11 +251,13 @@ namespace HavenSoft.HexManiac.Core.ViewModels {
          UpdateDexConversionTable.Run(model, token);
       }
 
-      private IList<int> Invert(IList<int> input, IList<int> filler) {
+      private IList<int> Invert(IReadOnlyList<SortablePokemon> list, IList<int> filler) {
          var result = new int[filler.Count];
          for (int i = 0; i < filler.Count; i++) result[i] = filler[i];
-         for (int i = 0; i < input.Count; i++) {
-            result[input[i] - 1] = i + 1;
+         for (int i = 0; i < list.Count; i++) {
+            if (list[i].CanonicalIndex == 0) continue; // skip this one
+            result[list[i].CanonicalIndex - 1] = i + 1;
+            foreach (var index in list[i].ExtraIndices) result[index - 1] = i + 1;
          }
          return result;
       }
@@ -257,8 +268,10 @@ namespace HavenSoft.HexManiac.Core.ViewModels {
 
    public class SortablePokemon : ViewModelCore, IPixelViewModel {
       private readonly IList<string> filterTerms;
+      private readonly List<int> extraIndices;
 
       public int CanonicalIndex { get; }
+      public IReadOnlyList<int> ExtraIndices => extraIndices;
       public int PixelWidth { get; }
       public int PixelHeight { get; }
       public short[] PixelData { get; }
@@ -275,6 +288,7 @@ namespace HavenSoft.HexManiac.Core.ViewModels {
          var palettes = new ModelTable(model, model.GetAddressFromAnchor(new NoDataChangeDeltaModel(), -1, HardcodeTablesModel.PokePalettesTable));
          var sprite = sprites[index].GetSprite(sprites[index].GetFieldName(0));
          var palette = palettes[index].GetPalette(palettes[index].GetFieldName(0));
+         extraIndices = new List<int>();
 
          CanonicalIndex = index;
          PixelWidth = sprite.GetLength(0);
@@ -283,6 +297,10 @@ namespace HavenSoft.HexManiac.Core.ViewModels {
          SpriteScale = 1;
 
          filterTerms = GenerateFilterTerms(model, index);
+      }
+
+      public void AddSource(int canonicalIndex) {
+         extraIndices.Add(canonicalIndex);
       }
 
       public void MatchToFilter(string filter) {
