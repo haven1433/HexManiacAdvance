bcc9f316f01878ad54cacf21fcf30c61bf009e94
Benjamin Popp <haven1433@gmail.com>
2020-10-01 12:56:37 -0500

    TableStreamRun expansion update
    Write from the last element instead of writing zero.

--- src/HexManiac.Core/Models/Runs/TableStreamRun.cs
+++ src/HexManiac.Core/Models/Runs/TableStreamRun.cs
@@ -318,8 +318,8 @@ namespace HavenSoft.HexManiac.Core.Models.Runs {
 
       public TableStreamRun Append(TableStreamRun run, ModelDelta token, int length) {
          var naturalLength = run.Length - EndCode.Count;
-         var newRun = (TableStreamRun)model.RelocateForExpansion(token, run, naturalLength + length * run.ElementLength + EndCode.Count);
-         for (int i = 0; i < run.ElementLength * length; i++) token.ChangeData(model, newRun.Start + naturalLength + i, 0x00);
+         var newRun = model.RelocateForExpansion(token, run, naturalLength + length * run.ElementLength + EndCode.Count);
+         for (int i = 0; i < run.ElementLength * length; i++) token.ChangeData(model, newRun.Start + naturalLength + i, model[newRun.Start + naturalLength + i - run.ElementLength]);
          for (int i = naturalLength + length * run.ElementLength; i < naturalLength; i++) if (model[newRun.Start + i] != 0xFF) token.ChangeData(model, newRun.Start + i, 0xFF);
          for (int i = 0; i < EndCode.Count; i++) token.ChangeData(model, newRun.Start + naturalLength + length * run.ElementLength + i, EndCode[i]);
          return new TableStreamRun(model, newRun.Start, run.PointerSources, run.FormatString, run.ElementContent, this);
