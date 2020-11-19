49f00ada89d482a33930ba60971f580b18e9c2cb
Benjamin Popp <haven1433@gmail.com>
2020-11-18 22:03:09 -0600

    Implement multi-column support for table tool
    This is something I've been thinking about doing for a while. Now, by toggling just a bit of Xaml, it's possible to support a dynamic multi-column layout within the table tool. Only works when you have multiple tables with linked length, such as with pokemon.

---
+++ src/HexManiac.WPF/Controls/ColumnStackPanel.cs
@@ -0,0 +1,160 @@
+﻿using HavenSoft.HexManiac.Core;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace HavenSoft.HexManiac.WPF.Controls {
+   public class ColumnStackPanel : Panel {
+      /// <summary>
+      /// During Measure, this grows based on the number of Headers found.
+      /// During Arrange, this limits the maximum number of columns.
+      /// </summary>
+      private int expectedHeaderCount = 0;
+
+      #region IsHeader
+
+      /// <summary>
+      /// Attached property for children within the panel. Set to 'true' in order to move to the next column.
+      /// </summary>
+      public static readonly DependencyProperty IsHeaderProperty = DependencyProperty.RegisterAttached("IsHeader", typeof(bool), typeof(ColumnStackPanel), new PropertyMetadata(false));
+
+      public static bool GetIsHeader(DependencyObject depObj) => (bool)depObj.GetValue(IsHeaderProperty);
+      public static void SetIsHeader(DependencyObject depObj, bool value) => depObj.SetValue(IsHeaderProperty, value);
+
+      #endregion
+
+      #region MinimumColumnWidth
+
+      /// <summary>
+      /// The minimum width needed for a column. The number of columns will scale based on available width.
+      /// </summary>
+      public static readonly DependencyProperty MinimumColumnWidthProperty = DependencyProperty.Register(nameof(MinimumColumnWidth), typeof(double), typeof(ColumnStackPanel), new FrameworkPropertyMetadata(32d, MinimumColumnWidthChanged));
+
+      public double MinimumColumnWidth {
+         get => (double)GetValue(MinimumColumnWidthProperty);
+         set => SetValue(MinimumColumnWidthProperty, value);
+      }
+
+      private static void MinimumColumnWidthChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+         var self = (ColumnStackPanel)d;
+         self.OnMinimumColumnWidthChanged(e);
+      }
+
+      protected virtual void OnMinimumColumnWidthChanged(DependencyPropertyChangedEventArgs e) => InvalidateMeasure();
+
+      #endregion
+
+      #region ColumnMargin
+
+      /// <summary>
+      /// The space between columns. Should be left empty.
+      /// </summary>
+      public static readonly DependencyProperty ColumnMarginProperty = DependencyProperty.Register(nameof(ColumnMargin), typeof(double), typeof(ColumnStackPanel), new FrameworkPropertyMetadata(0d, ColumnMarginChanged));
+
+      public double ColumnMargin {
+         get => (double)GetValue(ColumnMarginProperty);
+         set => SetValue(ColumnMarginProperty, value);
+      }
+
+      private static void ColumnMarginChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+         var self = (ColumnStackPanel)d;
+         self.OnColumnMarginChanged(e);
+      }
+
+      protected virtual void OnColumnMarginChanged(DependencyPropertyChangedEventArgs e) { }
+
+      #endregion
+
+      #region HeaderMargin
+
+      /// <summary>
+      /// The space between the end of one section and the start of a new section below it within the same column.
+      /// </summary>
+      public static readonly DependencyProperty HeaderMarginProperty = DependencyProperty.Register(nameof(HeaderMargin), typeof(double), typeof(ColumnStackPanel), new FrameworkPropertyMetadata(0d, HeaderMarginChanged));
+
+      public double HeaderMargin {
+         get => (double)GetValue(HeaderMarginProperty);
+         set => SetValue(HeaderMarginProperty, value);
+      }
+
+      private static void HeaderMarginChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+         var self = (ColumnStackPanel)d;
+         self.OnHeaderMarginChanged(e);
+      }
+
+      protected virtual void OnHeaderMarginChanged(DependencyPropertyChangedEventArgs e) { }
+
+      #endregion
+
+      protected override Size MeasureOverride(Size availableSize) {
+         // count header elements among children
+         expectedHeaderCount = 0;
+         foreach (UIElement child in InternalChildren) { if (GetContentIsHeader(child)) expectedHeaderCount += 1; }
+
+         var (widthPerColumn, desiredColumnCount) = CalculateColumnWidth(availableSize, expectedHeaderCount);
+         var offerSize = new Size(widthPerColumn, availableSize.Height);
+
+         // calculate the desired height of each column
+         var desiredHeights = new double[desiredColumnCount];
+         var activeColumn = -1;
+         foreach (UIElement child in InternalChildren) {
+            if (GetContentIsHeader(child)) {
+               activeColumn = (activeColumn + 1) % desiredColumnCount; // desiredHeights.IndexOf(desiredHeights.Min());
+               if (desiredHeights[activeColumn] > 0) desiredHeights[activeColumn] += HeaderMargin;
+            }
+            if (activeColumn < 0) activeColumn = 0;
+            child.Measure(offerSize);
+            desiredHeights[activeColumn] += child.DesiredSize.Height;
+         }
+
+         return new Size(widthPerColumn * desiredColumnCount + ColumnMargin * (desiredColumnCount - 1), desiredHeights.Max());
+      }
+
+      protected override Size ArrangeOverride(Size finalSize) {
+         var (widthPerColumn, desiredColumnCount) = CalculateColumnWidth(finalSize, expectedHeaderCount);
+
+         // calculate the desired height of each column
+         var usedHeight = new double[desiredColumnCount];
+         var activeColumn = -1;
+         foreach (UIElement child in InternalChildren) {
+            if (GetContentIsHeader(child)) {
+               activeColumn = (activeColumn + 1) % desiredColumnCount; // usedHeight.IndexOf(usedHeight.Min());
+               if (usedHeight[activeColumn] > 0) usedHeight[activeColumn] += HeaderMargin;
+            }
+            if (activeColumn < 0) activeColumn = 0;
+            child.Arrange(new Rect(activeColumn * (widthPerColumn + ColumnMargin), usedHeight[activeColumn], widthPerColumn, child.DesiredSize.Height));
+            usedHeight[activeColumn] += child.RenderSize.Height;
+         }
+
+         return new Size(widthPerColumn * desiredColumnCount + ColumnMargin * (desiredColumnCount - 1), usedHeight.Max());
+      }
+
+      private bool GetContentIsHeader(UIElement child) {
+         while (
+            child is ContentPresenter contentPresenter &&
+            !GetIsHeader(child) &&
+            VisualTreeHelper.GetChildrenCount(child) > 0
+         ) {
+            child = VisualTreeHelper.GetChild(child, 0) as UIElement;
+         }
+
+         return GetIsHeader(child);
+      }
+
+      private (double columnWidth, int columnCount) CalculateColumnWidth(Size offer, int maxColumns = int.MaxValue) {
+         // make sure we have a reasonable size to work with
+         var columnWidth = Math.Max(MinimumColumnWidth, 16);
+         var availableWidth = offer.Width;
+         if (double.IsNaN(availableWidth) || double.IsPositiveInfinity(availableWidth)) availableWidth = columnWidth * 2;
+         var desiredColumnCount = (int)((availableWidth + ColumnMargin) / (columnWidth + ColumnMargin));
+         if (maxColumns < 1) maxColumns = 1;
+         desiredColumnCount = Math.Min(Math.Max(1, desiredColumnCount), maxColumns);
+         if (desiredColumnCount < 1) desiredColumnCount = 1;
+         var widthPerColumn = availableWidth / desiredColumnCount;
+         widthPerColumn -= ColumnMargin;
+         return (widthPerColumn, desiredColumnCount);
+      }
+   }
+}
