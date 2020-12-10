185809eaf23cdedadb6e82c87f3dc5daaf3f430d
Benjamin Popp <haven1433@gmail.com>
2020-12-09 23:54:13 -0600

    bugfix
    clear existing format when adding a script run

--- src/HexManiac.Core/Models/Runs/Factory/RunStrategy.cs
+++ src/HexManiac.Core/Models/Runs/Factory/RunStrategy.cs
@@ -100,7 +100,11 @@ namespace HavenSoft.HexManiac.Core.Models.Runs.Factory {
       public override bool Matches(IFormattedRun run) => run is XSERun;
 
       public override bool TryAddFormatAtDestination(IDataModel owner, ModelDelta token, int source, int destination, string name, IReadOnlyList<ArrayRunElementSegment> sourceSegments, int parentIndex) {
-         throw new System.NotImplementedException();
+         var run = new XSERun(destination, new SortedSpan<int>(source));
+         if (run.Length < 1) return false;
+         owner.ClearFormat(token, run.Start, run.Length);
+         owner.ObserveRunWritten(token, run);
+         return true;
       }
 
       // TODO
@@ -126,6 +130,7 @@ namespace HavenSoft.HexManiac.Core.Models.Runs.Factory {
       public override bool TryAddFormatAtDestination(IDataModel owner, ModelDelta token, int source, int destination, string name, IReadOnlyList<ArrayRunElementSegment> sourceSegments, int parentIndex) {
          var run = new BSERun(destination, new SortedSpan<int>(source));
          if (run.Length < 1) return false;
+         owner.ClearFormat(token, run.Start, run.Length);
          owner.ObserveRunWritten(token, run);
          return true;
       }
@@ -153,6 +158,7 @@ namespace HavenSoft.HexManiac.Core.Models.Runs.Factory {
       public override bool TryAddFormatAtDestination(IDataModel owner, ModelDelta token, int source, int destination, string name, IReadOnlyList<ArrayRunElementSegment> sourceSegments, int parentIndex) {
          var run = new ASERun(destination, new SortedSpan<int>(source));
          if (run.Length < 1) return false;
+         owner.ClearFormat(token, run.Start, run.Length);
          owner.ObserveRunWritten(token, run);
          return true;
       }
