d4bec8c8feffe1dc801974ac99a78a7a0e56ffae
Benjamin Popp <haven1433@gmail.com>
2021-10-29 09:00:50 -0500

    Initial implementation of red-black tree
    Includes basic node-based implementation for 'Add'

---
+++ src/HexManiac.Core/Models/SearchTree.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HavenSoft.HexManiac.Core.Models {
+   public interface ISearchTreePayload {
+      int Start { get; }
+   }
+
+   public enum TreeColor { Red, Black }
+   public enum AddType { Left, Balanced, Right, Insert }
+
+   // Red-Black tree rules:
+   // (1) The root is black
+   // (2) All red nodes have black parents
+   // (3) The path to every limb has the same number of blacks.
+
+   /// <summary>
+   /// Implements a sorted collection with methods for storing and retrieving elements.
+   /// Unlike a SortedDictionary<>, you can get a value using a key that's not in the dictionary.
+   /// In such a case, you get the value with the _next_ start position.
+   /// </summary>
+   public class SearchTree {
+   }
+
+   public static class TreeNode {
+      public static TreeNode<T> From<T>(T element) where T : ISearchTreePayload => new TreeNode<T>(element);
+      public static void Add<T>(ref TreeNode<T> node, T element) where T : ISearchTreePayload => TreeNode<T>.Add(ref node, element);
+      public static void RotateRight<T>(ref TreeNode<T> node) where T : ISearchTreePayload {
+         var newRoot = node.Left;
+         node.Left = newRoot.Right;
+         newRoot.Right = node;
+         node = newRoot;
+      }
+      public static void RotateLeft<T>(ref TreeNode<T> node) where T : ISearchTreePayload {
+         var newRoot = node.Right;
+         node.Right= newRoot.Left;
+         newRoot.Left = node;
+         node = newRoot;
+      }
+      public static bool IsBlack<T>(this TreeNode<T> node) where T : ISearchTreePayload => (node?.Color ?? TreeColor.Black) == TreeColor.Black;
+   }
+
+   public class TreeNode<T> : IEnumerable<TreeNode<T>> where T : ISearchTreePayload {
+      public T Payload { get; }
+      public TreeColor Color { get; private set; }
+      public int BlackCount => ((Left == null) ? 0 : Left.BlackCount) + (Color == TreeColor.Black ? 1 : 0);
+      private TreeNode<T> left, right;
+      public TreeNode<T> Left { get => left; set => left = value; }
+      public TreeNode<T> Right { get => right; set => right = value; }
+      public TreeNode(T value) => Payload = value;
+
+      /// <summary>
+      /// Only do tree-balancing if the current node is red.
+      /// Returns true if the higher level should perform balancing.
+      /// </summary>
+      public static AddType Add(ref TreeNode<T> node, T element) {
+         var insert = TreeNode.From(element);
+         return Add(ref node, insert);
+      }
+
+      public static AddType Add(ref TreeNode<T> node, TreeNode<T> insert) {
+         var start = insert.Payload.Start;
+         if (node == null || node.Payload.Start == start) {
+            insert.Color = node?.Color ?? TreeColor.Red;
+            node = insert;
+            return AddType.Insert;
+         } else if (node.Payload.Start > start) {
+            var addMethod = Add(ref node.left, insert);
+            if (addMethod == AddType.Insert) return AddType.Left;
+            if (addMethod != AddType.Balanced) {
+               node.left.RecolorIfSelfAndSiblingAreRed(node.right, node);
+               if (addMethod == AddType.Right && !node.left.IsBlack() && node.right.IsBlack()) TreeNode.RotateLeft(ref node.left);
+               node.left.RotateRightIfSelfRedWithBlackSibling(node.right, ref node);
+            }
+         } else if (node.Payload.Start < start) {
+            var addMethod = Add(ref node.right, insert);
+            if (addMethod == AddType.Insert) return AddType.Right;
+            if (addMethod != AddType.Balanced) {
+               if (node.right.RecolorIfSelfAndSiblingAreRed(node.left, node)) return AddType.Insert;
+               if (addMethod == AddType.Left && !node.right.IsBlack() && node.left.IsBlack()) TreeNode.RotateRight(ref node.right);
+               node.right.RotateLeftIfSelfRedWithBlackSibling(node.left, ref node);
+            }
+         } else {
+            throw new NotImplementedException();
+         }
+
+         return AddType.Balanced;
+      }
+
+      public void Recolor() => Color = Color == TreeColor.Red ? TreeColor.Black : TreeColor.Red;
+
+      public IEnumerator<TreeNode<T>> GetEnumerator() {
+         if (left != null) {
+            foreach (var node in left) {
+               yield return node;
+            }
+         }
+         yield return this;
+         if (right != null) {
+            foreach (var node in right) {
+               yield return node;
+            }
+         }
+      }
+
+      IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+      private bool RecolorIfSelfAndSiblingAreRed(TreeNode<T> sibling, TreeNode<T> parent) {
+         if (!this.IsBlack() && !sibling.IsBlack()) {
+            // parent is guaranteed to be black by rule (2)
+            parent.Recolor();
+            this.Recolor();
+            sibling.Recolor();
+            return true;
+         }
+         return false;
+      }
+
+      private void RotateLeftIfSelfRedWithBlackSibling(TreeNode<T> sibling, ref TreeNode<T> parent) {
+         if (parent == null) return; // root is always black
+         if (!sibling.IsBlack()) return;
+         if (this.IsBlack()) return;
+         TreeNode.RotateLeft(ref parent);
+         parent.left.Recolor();
+         parent.Recolor();
+      }
+
+      private void RotateRightIfSelfRedWithBlackSibling(TreeNode<T> sibling, ref TreeNode<T> parent) {
+         if (parent == null) return; // root is always black
+         if (!sibling.IsBlack()) return;
+         if (this.IsBlack()) return;
+         TreeNode.RotateRight(ref parent);
+         parent.right.Recolor();
+         parent.Recolor();
+      }
+   }
+}
---
+++ src/HexManiac.Tests/SearchTreeTests.cs
@@ -0,0 +1,312 @@
+﻿using HavenSoft.HexManiac.Core.Models;
+using System.Linq;
+using Xunit;
+using static HavenSoft.HexManiac.Core.Models.TreeColor;
+
+namespace HavenSoft.HexManiac.Tests {
+   public class TestPayload : ISearchTreePayload {
+      public int Start { get; }
+      public int ID { get; }
+      public TestPayload(int start, int id) => (Start, ID) = (start, id);
+   }
+
+   public class SearchTreeTests {
+      private static TestPayload New(int value, int id = 0) => new TestPayload(value, id);
+      private static TreeNode<TestPayload> Node(int value, int id = 0) => new TreeNode<TestPayload>(New(value, id));
+      private static TreeNode<TestPayload> BlackNode(int value, int id = 0, TreeNode<TestPayload> left = null, TreeNode<TestPayload> right = null) {
+         var node = Node(value, id);
+         node.Recolor();
+         (node.Left, node.Right) = (left, right);
+         return node;
+      }
+
+      [Fact]
+      public void Element_LessValue_InsertedLeft() {
+         var node = BlackNode(5);
+         TreeNode.Add(ref node, New(4));
+
+         Assert.Equal(4, node.Left.Payload.Start);
+         Assert.Null(node.Right);
+         Assert.Equal(5, node.Payload.Start);
+         Assert.Equal(Black, node.Color);
+         Assert.Equal(Red, node.Left.Color);
+      }
+
+      [Fact]
+      public void Element_MoreValue_InsertedRight() {
+         var node = BlackNode(5);
+         TreeNode.Add(ref node, New(6));
+
+         Assert.Equal(6, node.Right.Payload.Start);
+         Assert.Null(node.Left);
+         Assert.Equal(5, node.Payload.Start);
+         Assert.Equal(Black, node.Color);
+         Assert.Equal(Red, node.Right.Color);
+      }
+
+      [Fact]
+      public void Element_SameValue_Replaces() {
+         var node = BlackNode(5);
+         TreeNode.Add(ref node, New(5, 1));
+
+         Assert.Equal(5, node.Payload.Start);
+         Assert.Equal(1, node.Payload.ID);
+         Assert.Null(node.Left);
+         Assert.Null(node.Right);
+         Assert.Equal(Black, node.Color);
+      }
+
+      [Fact]
+      public void RotateRight_PreservesChildren() {
+         //    y        x
+         //  x   c -> a   y
+         // a b          b c
+         var (a, x, b, y, c) = (Node(1), Node(2), Node(3), Node(4), Node(5));
+         (x.Left, x.Right) = (a, b);
+         (y.Left, y.Right) = (x, c);
+
+         var node = y;
+         TreeNode.RotateRight(ref node);
+
+         Assert.Equal(node, x);
+         Assert.Equal(x.Left, a);
+         Assert.Equal(x.Right, y);
+         Assert.Equal(y.Left, b);
+         Assert.Equal(y.Right, c);
+      }
+
+      [Fact]
+      public void RotateLeft_PreservesChildren() {
+         //    x        y
+         //  a   y -> x   c
+         //     b c  a b
+         var (a, x, b, y, c) = (Node(1), Node(2), Node(3), Node(4), Node(5));
+         (x.Left, x.Right) = (a, y);
+         (y.Left, y.Right) = (b, c);
+
+         var node = x;
+         TreeNode.RotateLeft(ref node);
+
+         Assert.Equal(node, y);
+         Assert.Equal(x.Left, a);
+         Assert.Equal(x.Right, b);
+         Assert.Equal(y.Left, x);
+         Assert.Equal(y.Right, c);
+      }
+
+      [Fact]
+      public void RedNode_Recolor_Black() {
+         var node = Node(1);
+
+         node.Recolor();
+         Assert.Equal(Black, node.Color);
+
+         node.Recolor();
+         Assert.Equal(Red, node.Color);
+      }
+
+      [Fact]
+      public void LeftChild_AddLeft_Recursive() {
+         var node = BlackNode(4);
+         node.Left = BlackNode(3);
+
+         TreeNode.Add(ref node, New(2));
+
+         Assert.Equal(2, node.Left.Left.Payload.Start);
+      }
+
+      [Fact]
+      public void LeftChild_AddRight_Recursive() {
+         var node = BlackNode(4);
+         node.Left = BlackNode(2);
+
+         TreeNode.Add(ref node, New(3));
+
+         Assert.Equal(3, node.Left.Right.Payload.Start);
+      }
+
+      [Fact]
+      public void RightChild_AddRight_Recursive() {
+         var node = BlackNode(4);
+         node.Right = BlackNode(5);
+
+         TreeNode.Add(ref node, New(6));
+
+         Assert.Equal(6, node.Right.Right.Payload.Start);
+      }
+
+      [Fact]
+      public void SelfAndSiblingRed_AddRed_SwapColorOfParentSelfAndSibling() {
+         var sibling = Node(1);
+         var node = Node(4);
+         var parent = BlackNode(2, left: sibling, right: node);
+
+         TreeNode.Add(ref parent, New(5));
+
+         // add to the right of node
+         // recolor node/sibling
+         // recolor the parent
+         Assert.Equal(Red, parent.Color);
+         Assert.Equal(Black, sibling.Color);
+         Assert.Equal(Black, node.Color);
+         Assert.Equal(Red, node.Right.Color);
+      }
+
+      [Fact]
+      public void SelfAndSiblingRed_AddRed_SwapColorOfParentSelfAndSibling2() {
+         var sibling = Node(1);
+         var node = Node(4);
+         var parent = BlackNode(2, left: sibling, right: node);
+
+         TreeNode.Add(ref parent, New(3));
+
+         // add to the left of node
+         // recolor node/sibling
+         // recolor the parent
+         Assert.Equal(Red, parent.Color);
+         Assert.Equal(Black, sibling.Color);
+         Assert.Equal(Black, node.Color);
+         Assert.Equal(Red, node.Left.Color);
+      }
+
+      [Fact]
+      public void RedWithLeftSiblingBlack_AddOuterRed_RotateLeft() {
+         var node = Node(4);
+         var parent = BlackNode(2, right: node);
+
+         TreeNode.Add(ref parent, New(5));
+
+         //   4B
+         // 2R  5R
+         Assert.Equal(Black, parent.Color);
+         Assert.Equal(Red, parent.Left.Color);
+         Assert.Equal(Red, parent.Right.Color);
+         Assert.Equal(5, parent.Right.Payload.Start);
+         Assert.Equal(node, parent);
+         Assert.Equal(2, parent.Left.Payload.Start);
+      }
+
+      [Fact]
+      public void RedWithRightSiblingBlack_AddOuterRed_RotateRight() {
+         var node = Node(2);
+         var parent = BlackNode(4, left: node);
+
+         TreeNode.Add(ref parent, New(1));
+
+         //   2B
+         // 1R  4R
+         Assert.Equal(Black, parent.Color);
+         Assert.Equal(Red, parent.Left.Color);
+         Assert.Equal(Red, parent.Right.Color);
+         Assert.Equal(1, parent.Left.Payload.Start);
+         Assert.Equal(node, parent);
+         Assert.Equal(4, parent.Right.Payload.Start);
+      }
+
+      [Fact]
+      public void RedWithLeftSiblingBlack_AddInnerRed_RotateLowerRightAndUpperLeft() {
+         var node = Node(4);
+         var parent = BlackNode(2, right: node);
+
+         TreeNode.Add(ref parent, New(3));
+
+         //   3B
+         // 2R  4R
+         Assert.Equal(Black, parent.Color);
+         Assert.Equal(Red, parent.Left.Color);
+         Assert.Equal(Red, parent.Right.Color);
+         Assert.Equal(2, parent.Left.Payload.Start);
+         Assert.Equal(3, parent.Payload.Start);
+         Assert.Equal(4, parent.Right.Payload.Start);
+         Assert.Equal(node, parent.Right);
+      }
+
+      [Fact]
+      public void RedWithRightSiblingBlack_AddInnerRed_RotateLowerLeftAndUpperRight() {
+         var node = Node(2);
+         var parent = BlackNode(4, left: node);
+
+         TreeNode.Add(ref parent, New(3));
+
+         //   3B
+         // 2R  4R
+         Assert.Equal(Black, parent.Color);
+         Assert.Equal(Red, parent.Left.Color);
+         Assert.Equal(Red, parent.Right.Color);
+         Assert.Equal(2, parent.Left.Payload.Start);
+         Assert.Equal(3, parent.Payload.Start);
+         Assert.Equal(4, parent.Right.Payload.Start);
+         Assert.Equal(node, parent.Left);
+      }
+
+      [Fact]
+      public void UnbalancedTree_AddNode_Balance() {
+         // initial state
+         //      2
+         //   1     (4)
+         //       3      6
+         //          (5)   (7)
+         //               add (8)
+         var tree = BlackNode(2, left: BlackNode(1), right: Node(4));
+         tree.Right.Left = BlackNode(3);
+         tree.Right.Right = BlackNode(6, left: Node(5), right: Node(7));
+
+         // first step: recolor to pull the black down
+         //      2
+         //   1     (4)
+         //       3     (6)
+         //           5     7 
+         //                   (8)
+         // but notice there's a double-red now
+         TreeNode.Add(ref tree, New(8));
+
+         // second step: rebalance, because 1/4 aren't both red
+         //     4
+         //  (2)  (6)
+         //  1 3  5 7
+         //         (8)
+         Assert.Equal(1, tree.Left.Left.Payload.Start);
+         Assert.Equal(2, tree.Left.Payload.Start);
+         Assert.Equal(3, tree.Left.Right.Payload.Start);
+         Assert.Equal(4, tree.Payload.Start);
+         Assert.Equal(5, tree.Right.Left.Payload.Start);
+         Assert.Equal(6, tree.Right.Payload.Start);
+         Assert.Equal(7, tree.Right.Right.Payload.Start);
+         Assert.Equal(8, tree.Right.Right.Right.Payload.Start);
+
+         var colors = tree.Select(node => node.Color).ToList();
+         Assert.Equal(new[] { Black, Red, Black, Black, Black, Red, Black, Red }, colors);
+      }
+
+      [Fact]
+      public void BalancedTree_AddNode_Recolor() {
+         // before:
+         //       4
+         //  (2)      (6)
+         // 1   3   5     8 
+         //            (7) (9)
+         var parent = BlackNode(4, left: Node(2), right: Node(6));
+         parent.Right.Left = BlackNode(5);
+         parent.Right.Right = BlackNode(8, left: Node(7), right: Node(9));
+
+         // after inserting (X)
+         TreeNode.Add(ref parent, New(10));
+
+         //      (4)
+         //   2       6
+         // 1   3   5  (8)
+         //            7 9
+         //              (X)
+         Assert.Equal(4, parent.Payload.Start);
+         Assert.Equal(Red, parent.Color);
+         Assert.Equal(Black, parent.Right.Color);
+         Assert.Equal(Red, parent.Right.Right.Color);
+         Assert.Equal(Black, parent.Right.Right.Left.Color);
+         Assert.Equal(Black, parent.Right.Right.Right.Color);
+      }
+
+      // TODO remove node tests
+
+      // TODO tests on the SearchTree itself, not just nodes
+   }
+}
