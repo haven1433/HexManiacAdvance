617419b1fc62ccaf18decc9a550923d951362e15
Benjamin Popp <haven1433@gmail.com>
2021-11-18 14:56:08 -0600

    Squashed commit of the following:
    commit f6c725ba8a696552f4821955ea4d822d7e152b98
    
        cleanup
    
    commit 35b85c2d4849671aef541013b355ca963b348fd9
    
        Get the application running
    
        Everything seems to work now. But the dll/pdb/xml files can't be placed in another directory, making the directory 11 files wider than it was before. I don't know how to fix this.
    
    commit 6804101c94e537954fb7d81c630dfb4eda8157ed
    
        Remove old config files
    
        .net core doesn't need these
    
    commit 6450c023b6fce109cda1c8167c87d7e20c0a9813
    
        Fix warnings
    
    commit 51f6580e51b8cc40da9e3cab6c6fbe998dac4a2d
    
        Add WPF csproj
    
        * Debug.Listeners doesn't exist. Internet recommends Trace.Listeners.
        * Requires explicit main method. Adding a main method that launches the App.
        * Generating documentation files
    
    commit 61d234d50f2d8bcb560096ad512cf160b566bc98
    
        Fix upgrade test path
    
    commit d4ee80a7ef2a459c3c1bb6108fb3dfdeefbc327a
    
        Add missing files from test resources directory
    
    commit 7ac8022346d24b6f4af4cd34e0a6e957cc6a8d39
    
        Adding custom AutoImplement for net6
    
        AutoImplement works in .net 4, but needs to work in .net standard. Updating the project files to use net6 assemblies, and updating AutoImplement to be a net6 project.
    
    Date:   Mon Nov 15 21:52:35 2021 -0600
    
        Adding old versions of HM.Core and HM.Test for comparisons during the upgrade
    
    commit 7c4c79c9b6f3a8e09962269a68b3515b8b39bbb6
    
        Initial attempt at adding test project
    
        This doesn't work yet: autoimplement cannot generate source from HexManiac.Core, since HexManiac.Core is now net6 and AutoImplement is net4.
    
    Date:   Mon Nov 15 21:19:20 2021 -0600
    
        HexManiac.Core in net6

---
+++ src/AutoImplement.Delegation/TypeArrayEqualityComparer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HavenSoft.AutoImplement.Delegation;
+
+/// <summary>
+/// Provides a simple comparison implementation for enumerable things.
+/// Used for creating dictionaries of implementations for generic methods (for stubs).
+/// </summary>
+public class EnumerableEqualityComparer<T> : IEqualityComparer<IEnumerable<T>> {
+   /// <inheritdoc />
+   public bool Equals(IEnumerable<T> a, IEnumerable<T> b) => a.SequenceEqual(b);
+
+   /// <inheritdoc />
+   public int GetHashCode(IEnumerable<T> elements) {
+      uint code = 0;
+      foreach (var element in elements) {
+         code = ((code <<13) | (code >> 19)); // cyclic bit shift by a prime number
+         code ^= (uint)element.GetHashCode();
+      }
+
+      return (int)code;
+   }
+}
