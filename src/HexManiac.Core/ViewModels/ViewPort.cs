e8da82bc37f6e62bdfdcf0e9ec32799e2a9a1b02
Haven1433 <haven1433@gmail.com>
2022-02-01 20:55:28 -0600

    bugfix
    Only use possible matches for constants if there are actually any matches.

--- src/HexManiac.Core/ViewModels/ViewPort.cs
+++ src/HexManiac.Core/ViewModels/ViewPort.cs
@@ -185,7 +185,7 @@ namespace HavenSoft.HexManiac.Core.ViewModels {
          if (arg is string str) {
             var possibleMatches = Model.GetExtendedAutocompleteOptions(str);
             if (possibleMatches.Count == 1) str = possibleMatches[0];
-            else if (possibleMatches.All(match => Model.GetMatchedWords(match).Any())) str = possibleMatches[0];
+            else if (possibleMatches.Count > 1 && possibleMatches.All(match => Model.GetMatchedWords(match).Any())) str = possibleMatches[0];
             var words = Model.GetMatchedWords(str).Where(word => Model.GetNextRun(word).Length < 3).ToList();
             if (words.Count == 1) {
                selection.Goto.Execute(words[0]);
