1bff2a0b0b08d01e8f921526d0df307f83c5d327
Haven1433 <haven1433@gmail.com>
2022-04-10 22:00:41 -0500

    Display group header

--- src/HexManiac.Core/ViewModels/Tools/TableGroupViewModel.cs
+++ src/HexManiac.Core/ViewModels/Tools/TableGroupViewModel.cs
@@ -3,6 +3,7 @@
 namespace HavenSoft.HexManiac.Core.ViewModels.Tools {
    public class TableGroupViewModel : ViewModelCore {
       private string groupName;
+      public bool DisplayHeader => GroupName != "Other";
       public string GroupName { get => groupName; set => Set(ref groupName, value); }
       public ObservableCollection<IArrayElementViewModel> Members { get; } = new();
    }
