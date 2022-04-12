1d76f5f1b36d9b669bdc9e66a7033dc319ba1d3f
Haven1433 <haven1433@gmail.com>
2022-04-11 21:44:37 -0500

    notify change for DisplayHeader when GroupName changes

--- src/HexManiac.Core/ViewModels/Tools/TableGroupViewModel.cs
+++ src/HexManiac.Core/ViewModels/Tools/TableGroupViewModel.cs
@@ -4,7 +4,7 @@ namespace HavenSoft.HexManiac.Core.ViewModels.Tools {
    public class TableGroupViewModel : ViewModelCore {
       private string groupName;
       public bool DisplayHeader => GroupName != "Other";
-      public string GroupName { get => groupName; set => Set(ref groupName, value); }
+      public string GroupName { get => groupName; set => Set(ref groupName, value, old => NotifyPropertyChanged(nameof(DisplayHeader))); }
       public ObservableCollection<IArrayElementViewModel> Members { get; } = new();
    }
 }
