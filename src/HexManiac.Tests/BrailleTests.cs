2991dcb8efc6bb393678df1c35e03d1bf4918af0
Haven1433 <haven1433@gmail.com>
2022-09-04 23:36:39 -0500

    outline of braille tests

---
+++ src/HexManiac.Tests/BrailleTests.cs
@@ -0,0 +1,12 @@
+﻿namespace HavenSoft.HexManiac.Tests {
+   public class BrailleTests : BaseViewModelTestClass {
+      // TODO we can add a braille run
+      // TODO empty braille string should be 1 byte long and say ""
+      // TODO 2-byte braille visually starts with " in the first cell and has only " in the second cell
+      // TODO editing the last byte of braille expands it
+      // TODO repointing
+      // TODO lowercase letters work
+      // TODO unsupported characters are ignored during edit
+      // TODO copy/paste
+   }
+}
