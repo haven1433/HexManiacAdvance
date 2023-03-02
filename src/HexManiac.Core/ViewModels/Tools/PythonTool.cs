cd3d55aaec578b13ff1754cf80fef9cf90eae00f
haven1433 <haven1433@gmail.com>
2023-03-01 22:04:15 -0600

    update python tool initial text

--- src/HexManiac.Core/ViewModels/Tools/PythonTool.cs
+++ src/HexManiac.Core/ViewModels/Tools/PythonTool.cs
@@ -27,9 +27,13 @@ namespace HavenSoft.HexManiac.Core.ViewModels.Tools {
          Text = @"print('''
    Put python code here.
    Use 'editor' to access the EditorViewModel.
-   Use 'table' to access tables from the current tab.
+   Use a table name to access tables from the current tab.
    For example, try printing:
-      table['data.pokemon.names'][1]['name']
+      data.pokemon.names[1].name
+   Or try changing a table using a loop:
+
+   for mon in data.pokemon.stats:
+     mon.hp = 100
 ''')";
       }
 
