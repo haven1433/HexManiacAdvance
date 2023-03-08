97b3045c774a35ff3ff5a2ad05bd0a50d58a3989
haven1433 <haven1433@gmail.com>
2023-03-07 20:28:13 -0600

    don't build a blockmap sprite if the run has no pointer sources

--- src/HexManiac.Core/ViewModels/DataFormats.cs
+++ src/HexManiac.Core/ViewModels/DataFormats.cs
@@ -193,6 +193,7 @@ namespace HavenSoft.HexManiac.Core.ViewModels.DataFormats {
 
       public static IPixelViewModel BuildSprite(IDataModel model, BlockmapRun run) {
          if (!IDataModelExtensions.EditorReleased) return null;
+         if (run.PointerSources.IsNullOrEmpty()) return null;
          var primarySource = run.PointerSources[0];
          var blocks1 = new BlocksetModel(model, model.ReadPointer(primarySource + 4));
          var blocks2 = new BlocksetModel(model, model.ReadPointer(primarySource + 8));
