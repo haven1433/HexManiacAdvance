85ac81ff3e7a3e7584ed642415dc19e8caa3f57e
haven1433 <haven1433@gmail.com>
2023-03-21 19:59:24 -0500

    code cleanup

--- bump.py
+++ bump.py
@@ -1,4 +1,5 @@
-# python -m bump
+# python -m bump build
+# python -m bump update
 # python -m bump minor
 # python -m bump major
 import sys
@@ -6,11 +7,12 @@ import sys
 def main(arg):
     lines = []
     new_version = ''
-    with open('src/SharedAssemblyInfo.cs') as file:
+    file_path = 'src/SharedAssemblyInfo.cs'
+    with open(file_path) as file:
         for line in file:
             lines.append(line)
 
-    with open('src/SharedAssemblyInfo.cs', 'w') as file:
+    with open(file_path, 'w') as file:
         for line in lines:
             if not 'AssemblyVersion' in line and not 'AssemblyFileVersion' in line:
                 file.write(line)
@@ -19,15 +21,16 @@ def main(arg):
             version = parts[1].split('.')
             while len(version) < 4: version.append('0')
             i = 3
-            if arg == 'major': i = 1
-            if arg == 'minor': i = 2
+            if arg == 'major': i = 0
+            if arg == 'minor': i = 1
+            if arg == 'update': i = 2
             version[i] = str(int(version[i])+1)
             if i < 3: version[3] = '0'
             if i < 2: version[2] = '0'
             if version[3] == '0': del version[3]
-            parts[1] = '.'.join(version)
-            file.write('"'.join(parts))
             new_version = '.'.join(version)
+            parts[1] = new_version
+            file.write('"'.join(parts))
 
     return new_version
 
