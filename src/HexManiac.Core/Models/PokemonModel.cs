6a98cef7918f5e0a29e61bf12500a1d5de10109e
TheDiamondMiner <ramzan150350@gmail.com>
2023-03-31 22:22:35 +0530

    Added changes

--- src/HexManiac.Core/Models/PokemonModel.cs
+++ src/HexManiac.Core/Models/PokemonModel.cs
@@ -7,7 +7,6 @@ using HavenSoft.HexManiac.Core.ViewModels.Visitors;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
