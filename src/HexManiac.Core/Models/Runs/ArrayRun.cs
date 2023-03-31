a958b5b0f5624ad92b593eb312bd924811aa8050
haven1433 <haven1433@gmail.com>
2023-03-30 22:01:49 -0500

    when matching data within records, check the concrete type

--- src/HexManiac.Core/Models/Runs/ArrayRun.cs
+++ src/HexManiac.Core/Models/Runs/ArrayRun.cs
@@ -1474,6 +1474,16 @@ namespace HavenSoft.HexManiac.Core.Models.Runs {
       public static bool DataMatchesSegmentFormat(IDataModel owner, int start, ArrayRunElementSegment segment, FormatMatchFlags flags, IReadOnlyList<ArrayRunElementSegment> sourceSegments, int parentIndex, bool deepCheck = true) {
          if (start < 0 || start >= owner.Count - segment.Length) return false;
          Debug.Assert(sourceSegments.Contains(segment), "Expected segment to be one among sourceSegments.");
+
+         // if it's a record, we want to check the match against the concrete form
+         if (segment is ArrayRunRecordSegment recSeg) {
+            var segmentOffset = sourceSegments.Until(seg => seg == segment).Sum(seg => seg.Length);
+            var matchOffset = sourceSegments.Until(seg => seg.Name == recSeg.MatchField).Sum(seg => seg.Length);
+            var matchLength = sourceSegments.FirstOrDefault(seg => seg.Name == recSeg.MatchField)?.Length ?? 1;
+            var matchValue = owner.ReadMultiByteValue(start + matchOffset - segmentOffset, matchLength);
+            segment = recSeg.CreateConcrete(owner.FormatRunFactory, owner.TextConverter, matchValue);
+         }
+
          switch (segment.Type) {
             case ElementContentType.PCS:
                int readLength = PCSString.ReadString(owner, start, true, segment.Length);
