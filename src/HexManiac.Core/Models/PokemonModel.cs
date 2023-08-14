f2545cb088440083d199396c9c428529ae22ede0
haven1433 <haven1433@gmail.com>
2023-08-13 22:25:02 -0500

    don't crash if no strategy is found

--- src/HexManiac.Core/Models/PokemonModel.cs
+++ src/HexManiac.Core/Models/PokemonModel.cs
@@ -1465,7 +1465,7 @@ namespace HavenSoft.HexManiac.Core.Models {
                }
             }
             tableStrategy.UpdateNewRunFromPointerFormat(this, token, segment, segments, parentIndex, ref run);
-         } else {
+         } else if(strategy != null) {
             strategy.UpdateNewRunFromPointerFormat(this, token, segment.Name, segments, parentIndex, ref run);
          }
       }
