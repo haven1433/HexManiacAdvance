602951969b61af9a028a4ce3549ba610eec3244a
haven1433 <haven1433@gmail.com>
2023-10-01 09:52:25 -0500

    python tool splitter thickness

--- src/HexManiac.WPF/Windows/MainWindow.xaml.cs
+++ src/HexManiac.WPF/Windows/MainWindow.xaml.cs
@@ -448,7 +448,7 @@ namespace HavenSoft.HexManiac.WPF.Windows {
       // when the ViewModel changes its GotoControlViewModel subsystem, update the event handler
       private void ViewModelPropertyChanged(object sender, PropertyChangedEventArgs e) {
          if (e.PropertyName == nameof(ViewModel.ShowAutomationPanel)) {
-            TabContainer.ColumnDefinitions[1].Width = ViewModel.ShowAutomationPanel ? new GridLength(1) : new GridLength(0);
+            TabContainer.ColumnDefinitions[1].Width = ViewModel.ShowAutomationPanel ? new GridLength(PythonToolSplitter.Width) : new GridLength(0);
             TabContainer.ColumnDefinitions[2].Width = ViewModel.ShowAutomationPanel ? new GridLength(300) : new GridLength(0);
             if (ViewModel.ShowAutomationPanel) {
                FocusTextBox(PythonTool.InputBox.TransparentLayer);
