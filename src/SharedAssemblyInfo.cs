8e81968a50c45af0194e6edce964c36ad7b40f5f
haven1433 <haven1433@gmail.com>
2023-11-29 22:22:04 -0600

    version bump

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.5.4.2")]
-[assembly: AssemblyFileVersion("0.5.4.2")]
+[assembly: AssemblyVersion("0.5.4.3")]
+[assembly: AssemblyFileVersion("0.5.4.3")]
 
 
 // AutoImplement style issues are expected, since it's compatible with earlier versions of C#.
