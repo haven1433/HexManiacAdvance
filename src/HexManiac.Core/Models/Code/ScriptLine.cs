8ce21a3e959a0c1da541857dadab0a2d85622850
haven1433 <haven1433@gmail.com>
2024-05-30 11:12:48 -0500

    validate that script macros that expect the same value in multiple args only apply if all the args actually have the same value

--- src/HexManiac.Core/Models/Code/ScriptLine.cs
+++ src/HexManiac.Core/Models/Code/ScriptLine.cs
@@ -160,12 +160,21 @@ namespace HavenSoft.HexManiac.Core.Models.Code {
       public bool Matches(int gameCodeHash, IReadOnlyList<byte> data, int index) {
          if (Args.Count == 0) return false;
          if (!MatchesGame(gameCodeHash)) return false;
+         var expectedVariableValues = new Dictionary<string, int>();
          for (int i = 0; i < Args.Count; i++) {
             var arg = Args[i];
             if (arg is SilentMatchArg smarg) {
                if (data[index] != smarg.ExpectedValue) return false;
             } else if (arg is ScriptArg sarg) {
-               // don't validate, this part is variable
+               // if the argument is duplicated through multiple spots,
+               // make sure all the spots match the same value.
+               var matchingShortArg = ShortFormArgs.FirstOrDefault(shortArg => shortArg.Name == arg.Name);
+               var argLength = arg.Length(default, -1);
+               if ((matchingShortArg?.Length(default, -1) ?? 0) == argLength) {
+                  var value = data.ReadMultiByteValue(index, argLength);
+                  if (!expectedVariableValues.TryGetValue(sarg.Name, out var expectedValue)) expectedVariableValues[sarg.Name] = value;
+                  else if (expectedValue != value) return false; // only match the macro if all the variables match
+               }
             } else {
                throw new NotImplementedException();
             }
